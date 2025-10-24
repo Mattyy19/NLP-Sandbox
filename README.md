4460c5818dc86378abc734b2777fdd80490b777d
Matthew Nguyen <mnguyen19045@gmail.com>
2025-10-23 21:11:21 -0700

    Added improved way to create training pairs for fine tuning. Updated README with necessary info.

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 ## NLP-Sandbox
 This repo will only be used to test and train different AI models. We are currently using "Wikipedia Top 100 Articles" as
-the main ZIM file for training. 
+the main ZIM file for training. The main branch is using all-MiniLM-L6-v2 as the pre-trained model.
 ### Dependencies Needed
 1. Python 3.10 or lower
 2. pyzim (python-zim when installing)
@@ -9,3 +9,4 @@ the main ZIM file for training.
 5. datasets
 6. torch
 7. transformers[torch]
+8. nltk
--- fine-tune/minilm-L6-v2-ft.py
+++ fine-tune/minilm-L6-v2-ft.py
@@ -2,14 +2,32 @@ from sentence_transformers import SentenceTransformer, InputExample, losses
 from datasets import load_dataset
 from torch.utils.data import DataLoader
 import random
+import nltk
+from nltk.tokenize import sent_tokenize
+nltk.download("punkt_tab")
 
 # Load dataset
 dataset = load_dataset("json", data_files="wikipedia_dataset.jsonl", split="train")
-texts = [d["text"] for d in dataset]
 
-# Create training pairs
-# [TODO]: Separate text into paragraphs to match with title
-tests = [InputExample(texts=[text, text]) for text in texts]
+# Creates training pairs for fine tuning
+tests = []
+for data in dataset:
+    try:
+        title = data.get("title", "")
+        text = data.get("text", "")
+
+        # Creates sentences out of the text, then every 3 sentences makes it a pair with the title
+        sentences = sent_tokenize(text)
+        for i in range(0, len(sentences), 3):
+            para = " ".join(sentences[i:i+3]).strip()
+            # Skips if paragraph is too short
+            if len(para) > 100:
+                tests.append(InputExample(texts=[title, para]))
+
+    except Exception as e:
+        print("Something went wrong with creating training pairs :(")
+        print(e)
+        continue
 random.shuffle(tests)
 
 # Load model and prepare for fine tuning
