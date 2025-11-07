33412b690e710e4bd2962d5b15e5b2a3f110829e
Matthew Nguyen <mnguyen19045@gmail.com>
2025-11-07 15:28:36 -0700

    Fixed how CPU usuage was being calculated

--- fine-tune/test_models.py
+++ fine-tune/test_models.py
@@ -4,16 +4,17 @@ import os
 import time
 
 # Loads fine-tuned model, change depending on which model
-model = SentenceTransformer("minilm-L6-v2_wikipedia100_ft")
+model = SentenceTransformer("minilm-L6-v2_wp_100_ft")
 
 # Gets system process info to track cpu + ram
 process = psutil.Process(os.getpid())
+cpu_count = psutil.cpu_count(logical=True)
 
 # Testing sample, change in the future
 corpus = [
-    "The 13th Amendment abolished slavery.",
-    "Evolution is the change in heritable characteristics.",
-    "Michael Jackson is a world-renowned pop star."
+    "The 13th Amendment abolished slavery, this was after the Civil War.",
+    "Evolution is the change in heritable characteristics, initially theorized by Charles Darwin.",
+    "Youtube is a website that allows people from around the world to watch and like videos, and subscribe to channels."
 ]
 corpus_embeddings = model.encode(corpus, convert_to_tensor=True)
 
@@ -22,11 +23,10 @@ while True:
     # Potential queries: What freed African Americans, How do animals change over time, Which singer made Thriller
     test = input("Please enter your search query (enter 'q' to quit): ")
     if test.lower() == 'q':
-        break;
+        break
 
     # Tracks time elapsed, cpu + ram usage
     start_time = time.time()
-    cpu_before = process.cpu_percent(interval=None)
     ram_before = process.memory_info().rss / 1024 ** 2
 
     # Embeds user input
@@ -38,7 +38,7 @@ while True:
 
     # Tracks time elapsed, cpu + ram usage
     end_time = time.time() - start_time
-    cpu_after = process.cpu_percent(interval=None)
+    cpu_usage = process.cpu_percent(interval=0.1) / cpu_count
     ram_after = process.memory_info().rss / 1024 ** 2
 
     # Displays results
@@ -49,5 +49,5 @@ while True:
     # Displays performance
     print("\nPerformance:")
     print(f"Elapsed time: {end_time:.4f} sec")
-    print(f"CPU usage: {cpu_after - cpu_before:.2f}%")
+    print(f"CPU usage: {cpu_usage:.2f}%")
     print(f"Memory used: {ram_after - ram_before:.2f} MB")
