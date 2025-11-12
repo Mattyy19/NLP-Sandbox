ec25307992497a10d6c8a9cc42237e34bc6e814e
galvar28 <galvar28@asu.edu>
2025-11-11 23:58:16 -0700

    Begun Task 87
    Begun implementing the script for this task, although still researching and devevloping, Code still in barebones.

---
+++ QueriesEmbedder.py
@@ -0,0 +1,22 @@
+#file created 11/11
+#add client to call query embedder
+#wire into kiwix search (Researching) so search can hit embedder
+#FastApi? 
+#will be continuing working, still researching and developing a solution
+#import os
+#import json
+#import time
+#from typing import Optional, List
+#import requests
+
+#file paths here
+#ENABLE = 
+#BASE = 
+#TO_MS = 
+#LOG = 
+
+#def _log(payload: dict) -> None:
+
+
+
+#def embed_query(text: str) -> Optional[List[float]]:
